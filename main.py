c9db158c51757b1a8cae06b414c5e6569298f1bc
Rui-Li-UCB <rui.li@ischool.berkeley.edu>
2023-03-06 16:38:00 -0800

    test2

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template
 
-app = Flask(__name__,template_folder='Templates')
+app = Flask(__name__,template_folder='Templates',static_folder = 'staticFiles')
 
 @app.route("/")
 def index():
