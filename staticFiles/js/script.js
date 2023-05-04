06fe00621d4ecebcd3d510c55472b15ea9a0dd00
Rui-Li-UCB <rui.li@ischool.berkeley.edu>
2023-05-03 17:41:46 -0700

    index_v3 update

--- staticFiles/js/script.js
+++ staticFiles/js/script.js
@@ -23,4 +23,17 @@ window.addEventListener('scroll', () => {
   else{
     cover.style.position = null
   }
-})
+});
+
+
+
+$ = function(id) {
+  return document.getElementById(id);
+}
+
+var show = function(id) {
+	$(id).style.display ='block';
+}
+var hide = function(id) {
+	$(id).style.display ='none';
+}
